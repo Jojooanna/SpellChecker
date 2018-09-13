31a04719e86a0701e422549f0e74cfc5d843cccd
Jojooanna <joannaompoc@gmail.com>
2018-09-13 15:38:22 +0800

    Open File Option Added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 import wx
 
 
 class Example(wx.Frame):
 
     def __init__(self, *args, **kwargs):
-        super(Example, self).__init__(*args, **kwargs)
+        super(Example, self).__init__(*args, **kwargs) #style=wx.TE_MULTILINE
+        self.inputtext = wx.TextCtrl(self, size = (800, 650))
 
         self.InitUI()
 
@@ -13,30 +15,54 @@ class Example(wx.Frame):
         panel = wx.Panel(self)
 
         menubar = wx.MenuBar()
-        NewMenu = wx.Menu()
-        ViewMenu = wx.Menu()
-        SusMenu = wx.Menu()
-        NewMenu.Append(wx.ID_NEW, '&New')
-        fileItem = NewMenu.Append(wx.ID_EXIT, 'Quit\tCtrl+Q', 'Quit application')
+        FileMenu = wx.Menu()
+
+        FileMenu.Append(wx.ID_NEW, '&New')
+        fileItem1 = FileMenu.Append(wx.ID_OPEN, '&Open')
+        FileMenu.AppendSeparator()
+
+        FileMenu.Append(wx.ID_SAVE, '&Save')
+        FileMenu.Append(wx.ID_SAVEAS, '&Save As')
+        FileMenu.AppendSeparator()
+
+        FileMenu.Append(wx.ID_EDIT, '&Rename')
+        FileMenu.AppendSeparator()
+
+        fileItem2 = FileMenu.Append(wx.ID_EXIT, 'Quit\tCtrl+Q', 'Quit application')
         # fileItem1 = NewMenu.Append(wx.ID_EXIT, 'New\tCtrl+F', 'Create New File')
 
-        menubar.Append(NewMenu, '&New')
-        menubar.Append(ViewMenu, '&Save')
+        ViewMenu = wx.Menu()
+        SusMenu = wx.Menu()
+
+        menubar.Append(FileMenu, '&File')
+        menubar.Append(ViewMenu, '&View')
         menubar.Append(SusMenu, '&sus')
         self.SetMenuBar(menubar)
 
 
-
-        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+        self.Bind(wx.EVT_MENU, self.OnOpen)
+        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem2)
         # self.Bind(wx.EVT_MENU, self.NewFile, fileItem1)
-
-        wx.TextCtrl(panel, pos = (10,10), size = (500, 450))
 
         self.SetSize((1200, 700))
         self.SetTitle('Filipino Spelling Checker')
         self.Centre()
 
 
+    def OnOpen(self, event):
+        wildcard = "TXT files (*.txt)|*.txt"
+        dialog = wx.FileDialog(self, "Open", wildcard=wildcard,
+                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        if dialog.ShowModal() == wx.ID_CANCEL:
+            return
+
+        path = dialog.GetPath()
+
+        if os.path.exists(path):
+            with open(path) as fileobject:
+                for line in fileobject:
+                    self.inputtext.WriteText(line)
 
     def OnQuit(self, e):
         self.Close()

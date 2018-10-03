3be3e62610e4177ef13600cd83c505c05467f7a8
mikeeevillanueva <mikeeevillanueva@gmail.com>
2018-10-04 00:20:17 +0800

    [New] controller file

---
+++ controller.py
@@ -0,0 +1,21 @@
+from model import *
+from sqlalchemy import create_engine
+
+def connectToDatabase():
+    """
+    Connect to our SQLite database and return a Session object
+    """
+    engine = create_engine("postgresql://postgres:mvjunetwo@localhost:5432/spell")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def addCommon(List):
+
+    session = connectToDatabase()
+
+    user = Common(List)
+    session.add(user)
+    session.commit()
+
+    print("common words added!")

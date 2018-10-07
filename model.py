2afbc138752c575bd13690cdd5b148e37841e516
mikeeevillanueva <mikeeevillanueva@gmail.com>
2018-10-08 02:53:43 +0800

    new model for all the words in dictionary for spelling checker

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy as db
+from sqlalchemy.dialects import postgresql as pg
 
 engine = create_engine('postgresql://postgres:jojo123@localhost:5432/postgres')
 Base = declarative_base()
@@ -11,7 +12,7 @@ class Words(Base):
 
     codeid = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String, nullable=False)
-    words = db.Column(db.ARRAY(String), nullable=False)
+    words = db.Column(pg.ARRAY(db.String), nullable=False)
 
     def __init__(self, code, words):
         self.code =code
@@ -27,5 +28,14 @@ class Common(Base):
         self.words = words
 
     # def _
+class inputWords(Base):
+    __tablename__ = "words"
+    wordid =Column(Integer, primary_key=True)
+    word = Column(String)
+
+    def __init__(self, word):
+        self.word = word
+
 Session = sessionmaker(engine)
 session = Session()
+session.commit()
